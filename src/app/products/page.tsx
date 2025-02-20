c74335797fb4001d73451fcc2aad67647ea15944
Liziero <iagolizierox14@gmail.com>
2025-02-20 02:08:52 -0300

    update: globalss

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,26 @@
-import { Select } from "@/components/ui/select";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
 
 const ProductPage = () => {
     return ( <div className="bg-gray-800 text-white mt-10 flex  flex-col justify-center items-center border-none">
         <h1 className="text-4xl "> olaaaa</h1>
         <div>
-            <select />
+        <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
         </div>
     </div> );
 }
