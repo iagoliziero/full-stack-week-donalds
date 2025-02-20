d1d61971bc02f3667f103928f5cdad2a1f284bca
Liziero <iagolizierox14@gmail.com>
2025-02-20 02:05:46 -0300

    feat: add shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Select } from "@/components/ui/select";
+
+const ProductPage = () => {
+    return ( <div className="bg-gray-800 text-white mt-10 flex  flex-col justify-center items-center border-none">
+        <h1 className="text-4xl "> olaaaa</h1>
+        <div>
+            <select />
+        </div>
+    </div> );
+}
+ 
+export default ProductPage ;
