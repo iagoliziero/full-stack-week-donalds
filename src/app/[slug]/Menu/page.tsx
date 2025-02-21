0b3782b8c784d9691da43af4658183bd360cc007
Liziero <iagolizierox14@gmail.com>
2025-02-21 02:26:08 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/Menu/page.tsx
+++ src/app/[slug]/Menu/page.tsx
@@ -1,39 +1,45 @@
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
-
-
-interface RestaurantMenuPage {
-    params: Promise<{slug: string}>;
-    searchParams: Promise<{consumptionMethod: string}>
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-    return ['DINE_IN', "TAKEAWAY"].includes(consumptionMethod)
-}
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
 
-const RestaurantMenuPage = async ({params, searchParams}: RestaurantMenuPage) => {
-    const {slug} = await params
-    const { consumptionMethod } = await searchParams;
-    if (!isConsumptionMethodValid(consumptionMethod)) {
-        return notFound()
-    }
-    const restaurant = await db.restaurant.findUnique({where: {slug}})
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
 
-    if (!restaurant) {
-        return notFound()
-    }
-
-    return ( <h1> ola
-        <div>
-            <RestaurantHeader restaurant={restaurant}/>
-        </div>
-    </h1>  );
-}
- 
 export default RestaurantMenuPage;
